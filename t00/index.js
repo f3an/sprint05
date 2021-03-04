b25c2f14decc1f5007daada8d2a42dc2daefd140
f3an <ichernyavs@student.ucode.world>
2021-03-04 12:48:40 +0200

    Good Morning

---
+++ t00/index.js
@@ -0,0 +1,8 @@
+function t00() {
+    console.log("Good morning!\n" +
+        "It's 7 A.M.\n" +
+        "The Weather in Malibu is 72 degrees with scatted clouds\n" +
+        "Thesurfconditionsarefair, hightidewillbe at 10:52a.m.\n"
+    );
+}
+t00();
